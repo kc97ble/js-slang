524fd6c14d2c3d5b232fdf3940f81ee3f866ade9
JackXTY <jack123qd@hotmail.com>
2020-04-10 23:02:15 +0800

    new test cases

--- src/stdlib/__tests__/thunk_list.ts
+++ src/stdlib/__tests__/thunk_list.ts
@@ -15,11 +15,11 @@ test('infinite functions with list', () => {
   return expectResult(
     stripIndent`
         function f(x) { return list(x,f(x+1)); }
-        head(f(0))+head(tail(tail(f(0))));
+        head(f(0))+head(head(tail(f(0))));
         `,
     { chapter: 2, native: false, lazyEvaluation: true }
   ).toMatchInlineSnapshot(`
-    2
+    1
     `)
 })
 
@@ -56,5 +56,54 @@ test('list_ref with infinite function', () => {
     { chapter: 2, native: false, lazyEvaluation: true }
   ).toMatchInlineSnapshot(`
     3
+    `)
+})
+
+test('map with infinite function', () => {
+  return expectResult(
+    stripIndent`
+        function f(x) { return list(x,f(x+1)); }
+        head(tail(map((a)=>{return a*a;}, f(1))));
+        `,
+    { chapter: 2, native: false, lazyEvaluation: true }
+  ).toMatchInlineSnapshot(`
+    4
+    `)
+})
+
+test('member with infinite function', () => {
+  return expectResult(
+    stripIndent`
+        function f(x) { return pair(x*x,f(x+1)); }
+        head(member(4,f(0)));
+        `,
+    { chapter: 2, native: false, lazyEvaluation: true }
+  ).toMatchInlineSnapshot(`
+    4
+    `)
+})
+
+test('remove_all with infinite function', () => {
+  return expectResult(
+    stripIndent`
+        function f(x) { return pair(x,f(x+1)); }
+        head(tail(remove_all(1,f(0))));
+        `,
+    { chapter: 2, native: false, lazyEvaluation: true }
+  ).toMatchInlineSnapshot(`
+    2
+    `)
+})
+
+test('filter with infinite function', () => {
+  return expectResult(
+    stripIndent`
+        function f(x) { return pair(x,f(x+1)); }
+        function h(x){return x%2===0;}
+        head(filter(h,f(1)));
+        `,
+    { chapter: 2, native: false, lazyEvaluation: true }
+  ).toMatchInlineSnapshot(`
+    2
     `)
 })
